392f6bee3971b55e5cd6d20248ca56967a9e7db4
Chingiz Akhmedov <c.akhmedov@itstep.edu.az>
2021-04-08 19:14:58 +0400

    Added ckass car

--- ProjectX/main.cpp
+++ ProjectX/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class Car
+{
+public:
+	Car()
+	{
+		name = "BMW";
+	}
+	void operator()(string _name) {
+		name = _name;
+	}
+	void Print() {
+		cout << name <<endl;
+	}
+private:
+	string name;
+};
+
+
+
+
+
 void main() {
 	cout << "Something" << endl;
 	cout << "Hello, world!" << endl;
-
+	Car car;
+	car.Print();
+	car("Mini");
+	car.Print();
 }
