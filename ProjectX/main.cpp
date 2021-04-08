571b470494492079488eb9709c95cfdcf115baae
NiiL2150 <nail.shamsudinov@gmail.com>
2021-04-08 20:52:48 +0400

    Added Nail class

--- ProjectX/main.cpp
+++ ProjectX/main.cpp
@@ -18,14 +18,30 @@ private:
 	string name;
 };
 
+class Nail {
+private:
+	string nickname;
+	int age;
+	int grade;
+public:
+	Nail(string _nickname, int _age, int _grade) : nickname(_nickname), age(_age), grade(_grade) {}
+	Nail() : Nail("NiiL2150", 15, 10) {};
 
+	string GetName() const { return nickname; }
+	void SetName(string _name) { nickname = _name; }
+
+	int GetAge() const { return age; }
+	void SetAge(int _age) { age = _age; }
+
+	int GetGrade() const { return grade; }
+	void SetGrade(int _grade) { grade = _grade; }
+
+	void Print() {
+		cout << "Nail\nNickname: " << nickname << endl << "Age: " << age << "\nGrade: " << grade << endl;
+	}
+};
 
 void main() {
-	cout << "Deploying microservices..." << endl;
-	Car car;
-	car.Print();
-	car("Mini");
-	car.Print();
-	cout << "Deploying end..."<< endl;
-
+	Nail nail;
+	nail.Print();
 }
